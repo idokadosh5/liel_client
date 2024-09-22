faf96723cf1f9543ecbcf39d22be65b1fdba936e
idokadosh5 <87426947+idokadosh5@users.noreply.github.com>
2024-09-22 22:07:56 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-});
+  optimizeDeps: {
+    disabled: true, // Disables the optimizer completely
+    // or
+    // disabled: 'dev', // Disables optimizer only during development
+  },
+})
+
